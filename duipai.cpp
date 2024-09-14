1f4a3e40982bceec62e8e41ed8f05d37a4134e34
JiZiQian <jzq20015005@outlook.com>
2024-09-14 22:17:47 +0800

    upd auto completion

--- duipai.cpp
+++ duipai.cpp
@@ -4,6 +4,7 @@
 #include <readline/history.h>
 #include "run.hpp"
 #include "base.hpp"
+#include "autoComplete.hpp"
 using namespace std;
 string author="wzh jzq";
 //diff
@@ -132,40 +133,7 @@ inline void querytest(vector<string> &arg)
 //clean
 inline void clean()
 {
-    system("ls | grep -Ev 'duipai|ans|my|make_data|README.md|install.sh|base.hpp|run.hpp' | xargs rm -r");
-}
-// init
-vector<string> commands={"comp","help","run","quit","test","clear","cat","clean"};
-vector<string> files={"ans","data","my","all"};
-vector<string> current_map;
-char *command_generator(const char *text,int state){
-    static int list_index,len;
-    string name;
-    if(!state) {
-        list_index=0;
-        len=strlen(text);
-    }
-    while(list_index<current_map.size()){
-        name=current_map[list_index++];
-        if (strncmp(name.c_str(), text, len) == 0){
-            char *t;
-            t=(char*)malloc(name.size()+1);
-            strcpy(t,name.c_str());
-            return t;
-        }
-    }
-    return (char *)NULL;
-}
-char **autoComplete(const char *text,int start,int end){
-    char **matches;
-    matches=(char**)NULL;
-    if(start==0) current_map=commands;
-    else current_map=files;
-    matches=rl_completion_matches(text,command_generator);
-    return matches;
-}
-void init(){
-    rl_attempted_completion_function=autoComplete;
+    system("ls | grep -Ev 'duipai|ans|my|make_data|README.md|install.sh|base.hpp|run.hpp|data' | xargs rm -r");
 }
 // console
 int main()
