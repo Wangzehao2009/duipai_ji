95eaad67588a832b2e2a0ea3625d753d8c81ec4e
JiZiQian <jzq20015005@outlook.com>
2024-11-10 13:26:46 +0800

    upd .gitignore

--- make_data.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#include "carefree.hpp"
-using namespace std;
-using namespace carefree;
-static auto __mkji_init__ = []() -> int {carefree_internal::init_rnd_seed(); return 0;}();
-int main()
-{
-    return 0;
-}
